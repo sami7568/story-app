b6301e1a55cf91648d56899bcec8cb4bc5bd81e5
sami <samijan7568@gmail.com>
2021-03-20 09:05:44 +0500

    story app

--- android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+        }
+
